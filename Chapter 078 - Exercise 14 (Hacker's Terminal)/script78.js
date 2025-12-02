235cb051e0c53309d2a04c4c0ca63ca2ec2d1b94
yash-2008-yash <yashwanthm897@gmail.com>
2025-12-02 15:20:01 +0530

    Updated

--- Chapter 078 - Exercise 14 (Hacker's Terminal)/script78.js
+++ Chapter 078 - Exercise 14 (Hacker's Terminal)/script78.js
@@ -1,111 +1,60 @@
-async function hackingSimulation() {
+let message1 = document.getElementById("message1")
+let message2 = document.getElementById("message2")
+let message3 = document.getElementById("message3")
+let message4 = document.getElementById("message4")
+let message5 = document.getElementById("message5")
+let result = document.getElementById("result")
+
+let delay1 = Math.floor(Math.random() * (7000 - 1000 + 1)) + 1000
+let delay2 = Math.floor(Math.random() * (7000 - 1000 + 1)) + 1000
+let delay3 = Math.floor(Math.random() * (7000 - 1000 + 1)) + 1000
+let delay4 = Math.floor(Math.random() * (7000 - 1000 + 1)) + 1000
+let delay5 = Math.floor(Math.random() * (7000 - 1000 + 1)) + 1000
+
+function Blink(element, text, delay) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            setTimeout(() => {
+                element.textContent = `${text}`
+            }, 100);
+
+            setTimeout(() => {
+                element.textContent = `${text}.`
+            }, 300);
+
+            setTimeout(() => {
+                element.textContent = `${text}..`
+            }, 600);
+
+            setTimeout(() => {
+                element.textContent = `${text}...`
+                resolve()
+            }, 900);
+        }, delay);
+    })
+}
+
+function endMessage() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            result.innerHTML = "HACKING COMPLETE."
+            resolve()
+        }, 500);
+    })
+}
+
+async function HackingSimulation() {
     try {
-        const delay1 = Math.floor(Math.random() * (7000 - 1000 + 1)) + 1000
-        const delay2 = Math.floor(Math.random() * (7000 - 1000 + 1)) + 1000
-        const delay3 = Math.floor(Math.random() * (7000 - 1000 + 1)) + 1000
-        const delay4 = Math.floor(Math.random() * (7000 - 1000 + 1)) + 1000
-        const delay5 = Math.floor(Math.random() * (7000 - 1000 + 1)) + 1000
-
-        const blinkIntervals = {}
-
-        function startBlinkingDots(elementID, baseText, speed = 350) {
-            const element = document.getElementById(elementID)
-
-            if (blinkIntervals[elementID]) {
-                clearInterval(blinkIntervals[elementID])
-            }
-
-            let dotCount = 0
-            element.textContent = baseText
-            const id = setInterval(() => {
-                dotCount = (dotCount + 1) % 4
-                element.textContent = baseText + ".".repeat(dotCount)
-            }, speed)
-
-            blinkIntervals[elementID] = id
-            return id
-        }
-
-        function stopBlinking(elementID, finalText) {
-            const element = document.getElementById(elementID)
-            if (blinkIntervals[elementID]) {
-                clearInterval(blinkIntervals[elementID])
-                delete blinkIntervals[elementID]
-            }
-            if (element)
-                element.textContent = finalText
-        }
-
-        function msg1() {
-            return new Promise((resolve) => {
-                startBlinkingDots("message1", "Initializing Hacking");
-                setTimeout(() => {
-                    stopBlinking("message1", "Initializing Hacking...");
-                    resolve();
-                }, delay1);
-            });
-        }
-
-        function msg2() {
-            return new Promise((resolve) => {
-                startBlinkingDots("message2", "Reading all the files");
-                setTimeout(() => {
-                    stopBlinking("message2", "Reading all the files...");
-                    resolve();
-                }, delay2);
-            });
-        }
-
-        function msg3() {
-            return new Promise((resolve) => {
-                startBlinkingDots("message3", "Password encryption cracked");
-                setTimeout(() => {
-                    stopBlinking("message3", "Password encryption cracked...");
-                    resolve();
-                }, delay3);
-            });
-        }
-
-        function msg4() {
-            return new Promise((resolve) => {
-                startBlinkingDots("message4", "Sending all your files to the server");
-                setTimeout(() => {
-                    stopBlinking("message4", "Sending all your files to the server...");
-                    resolve();
-                }, delay4);
-            });
-        }
-
-        function msg5() {
-            return new Promise((resolve) => {
-                startBlinkingDots("message5", "Cleaning up");
-                setTimeout(() => {
-                    stopBlinking("message5", "Cleaning up...");
-                    resolve();
-                }, delay5);
-            });
-        }
-
-        function result() {
-            return new Promise((resolve) => {
-                setTimeout(() => {
-                    document.getElementById("result").innerHTML = "Hacking Complete."
-                    resolve()
-                }, 2000)
-            })
-        }
-
-        await msg1()
-        await msg2()
-        await msg3()
-        await msg4()
-        await msg5()
-        await result()
+        await Blink(message1, "Initializing Hacking", delay1)
+        await Blink(message2, "Reading all the files", delay2)
+        await Blink(message3, "Password encryption cracked", delay3)
+        await Blink(message4, "Sending all your files to the server", delay4)
+        await Blink(message5, "Cleaning up", delay5)
+        await endMessage()
     }
-
     catch (error) {
-        console.error("Error: ", error)
+        console.log(error)
     }
 }
 
-hackingSimulation()
+HackingSimulation()
