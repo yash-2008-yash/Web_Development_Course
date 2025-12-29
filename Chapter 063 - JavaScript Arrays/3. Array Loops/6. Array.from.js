991f14910fba84be2045dbbbcfeab538bba13a54
Yashwanth M <yashwanthm897@gmail.com>
2025-12-30 00:00:43 +0530

    Fix typo in Array.from example
    Previously, the variable 'my_name' was used as 'my_namename'. Now, fixed that typo.

--- Chapter 063 - JavaScript Arrays/3. Array Loops/6. Array.from.js
+++ Chapter 063 - JavaScript Arrays/3. Array Loops/6. Array.from.js
@@ -1,7 +1,7 @@
 // Array.from(object) creates an array from any other object
 
 let my_name = "Yashwanth"
-console.log(Array.from(my_namename)) // ['Y','a','s','h','w','a','n','t','h']
+console.log(Array.from(my_name)) // ['Y','a','s','h','w','a','n','t','h']
 
 let array = Array.from([1, 2, 3], x => x ** 2)
 console.log(array) // [1, 4, 9]
