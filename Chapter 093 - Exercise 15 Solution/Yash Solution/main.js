d988cb3fe7e6657162446d92152cafe660aa04b2
yash-2008-yash <yashwanthm897@gmail.com>
2026-01-18 12:25:19 +0530

    Chapter 93 Completed

---
+++ Chapter 093 - Exercise 15 Solution/Yash Solution/main.js
@@ -0,0 +1,35 @@
+import fs from "fs"
+import path from "path"
+
+const directory = "/home/yashwanth-m/Files/Web Development Course/Chapter 093 - Exercise 15 Solution/Yash Solution"
+
+fs.readdir(directory, (error, files) => {
+    if (error) {
+        console.log(`${error}`)
+        return;
+    }
+
+    files.forEach(file => {
+        const extension = path.extname(file)
+
+        if (!extension) {
+            return;
+        }
+
+        const folderName = extension.slice(1)
+        const folderPath = path.join(directory, folderName)
+
+        if (!fs.existsSync(folderPath)) {
+            fs.mkdirSync(folderPath)
+        }
+
+        const source = path.join(directory, file)
+        const destination = path.join(folderPath, file)
+
+        fs.rename(source, destination, error => {
+            if (error) {
+                console.error(`${error}`)
+            }
+        })
+    })
+})
